0ef7ed67b36a1082c60783ad575ddbfc12718611
xt <khlopenuk.ta@gamil.com>
2017-03-30 16:13:23 +0500

    add simple phone validation

--- example/index.ts
+++ example/index.ts
@@ -1,3 +1,12 @@
-import PhoneValidation from '../src/phone-validate'
+import PhoneValidation from '../src/phone-validate';
 
-let phone = new PhoneValidation();
+let phoneCheck = new PhoneValidation();
+let phoneInput = <HTMLInputElement> document.querySelector('.phone');
+
+phoneInput.oninput = () => {
+	phoneInput.value = phoneCheck.validate(phoneInput.value);
+	console.log(phoneCheck.phone);
+}
+
+phoneInput.onclick = phoneCheck.addPhonePrefix;
+// phoneInput.onblur = phoneCheck.clear;
--- src/phone-validate.ts
+++ src/phone-validate.ts
@@ -1,8 +1,11 @@
 export default class PhoneValidation {
     private _phone: number;
     flag: boolean;
-    constructor(public limit=10, public spaces=[3, 7, 10]) {
-    }
+    constructor(
+        public limit=10, 
+        public spaces=[3, 7, 10], 
+        public preffix="+7 "
+    ) {}
 
     get phone(): string {
         return String(this._phone);
@@ -13,6 +16,21 @@ export default class PhoneValidation {
         if (newPhone.length <= this.limit) {
             this._phone = Number(newPhone);
         }
+    }
+    
+    /** get validated phone
+     * @param {string}
+     */
+    validate(val: string | number) {
+        this.phone = String(val);
+        return this.phone;
+    }
+    
+    /**
+     * add phone preffix
+     */
+    addPhonePrefix = function() {
+        this.flag = true;
     }
 }
 
